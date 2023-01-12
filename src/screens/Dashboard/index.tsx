daeb116bc4df879babd14bc237f4d8e486c7ead8
silassousa <silassousadejesus@gmail.com>
2023-01-11 21:11:00 -0300

    feat(Dashbord) tornando cards dinamicos

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { FlatList } from "react-native";
+
 import { HighlightCard } from "../components/HighlightCard";
-import { TransactionCard } from "../components/transactionCard";
+import {
+  TransactionCard,
+  TransactionCardProps,
+} from "../components/TransactionCard";
 import {
   Container,
   Header,
@@ -14,9 +19,50 @@ import {
   Icon,
   Transactions,
   Title,
+  TransactionList,
 } from "./styles";
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function Dashboard() {
+  const data: DataListProps[] = [
+    {
+      id: "1",
+      type: "positive",
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000, 00",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign",
+      },
+      date: "13/04/2020",
+    },
+    {
+      id: "2",
+      type: "negative",
+      title: "Hamburgueria Pizzy",
+      amount: "R$ 59, 00",
+      category: {
+        name: "Alimentação",
+        icon: "coffee",
+      },
+      date: "13/04/2020",
+    },
+    {
+      id: "3",
+      type: "negative",
+      title: "Aluguel do apartamento",
+      amount: "R$ 1.200,00",
+      category: {
+        name: "Casa",
+        icon: "shopping-bag",
+      },
+      date: "13/04/2020",
+    },
+  ];
+
   return (
     <Container>
       <Header>
@@ -44,7 +90,11 @@ export function Dashboard() {
 
       <Transactions>
         <Title>Listagem</Title>
-        <TransactionCard />
+        <TransactionList
+          data={data}
+          keyExtractor={(item: DataListProps) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
       </Transactions>
     </Container>
   );
--- src/screens/components/transactionCard/styles.ts
+++ src/screens/components/transactionCard/styles.ts
@@ -1,22 +1,30 @@
+import { Transactions } from "./../../Dashboard/styles";
 import styled from "styled-components/native";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import { Feather } from "@expo/vector-icons";
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
+interface TransactionProps {
+  type: "positive" | "negative";
+}
+
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.shape};
   border-radius: 5px;
   padding: 17px 24px;
+  margin-bottom: 16px;
 `;
 
 export const Title = styled.Text`
   font-size: ${RFValue(14)}px;
   font-family: ${({ theme }) => theme.fonts.regular};
 `;
-export const Amount = styled.Text`
+export const Amount = styled.Text<TransactionProps>`
   font-size: ${RFValue(20)}px;
   margin-top: 2px;
   font-family: ${({ theme }) => theme.fonts.regular};
+
+  color: ${({ theme, type }) =>  type === 'positive' ? theme.colors.sucess : theme.colors.attention};
 `;
 export const Footer = styled.View`
   flex-direction: row;
